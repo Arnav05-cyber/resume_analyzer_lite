c635e9ce369eafc54e81463d7e30be3f4d352216
Arnav <arnav.vyas06@gmail.com>
2025-07-31 15:12:26 +0530

    procfile dleted

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn resume_matcher_api:app --bind 0.0.0.0$PORT
--- resume_matcher_api.py
+++ resume_matcher_api.py
@@ -189,6 +189,8 @@ def match_resume_jd():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
+    
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
